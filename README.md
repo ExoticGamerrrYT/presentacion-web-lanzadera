f58292a76a554a1341591e639801b51d010bdf83
Exotic-Dev <ExoticGamerrrYT>
2023-12-24 19:23:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Presentación Web de una Lanzadera Espacial Móvil :rocket::truck:
+Hola! Este es el código de la web para presentar mi proyecto de Tecnología de 1º de bachillerato.
+
+### Licencia
+Bajo la [MIT LICENSE](./LICENSE).
+
+>[Ver sus características rápidamente.](https://choosealicense.com/licenses/mit/#)
+
+### Contacto
+Discord: `3xotic.`
---
+++ src/app.js
@@ -0,0 +1,22 @@
+var subtitle1 = document.querySelector("#subtitle-1");
+var subtitle1_text = 'Lanzadera de cohetes espaciales móvil.';
+// Cambiamos el contenido del subtítulo
+if (subtitle1) {
+    subtitle1.textContent = subtitle1_text;
+}
+var details1 = document.querySelector("#details-1");
+var details1_text = 'Trabajo de tecnología de 1º de bachillerato, consiste en modelar una lanzadera de cohetes espaciales en 3D.';
+if (details1) {
+    details1.textContent = details1_text;
+}
+/* CHECKBOX
+function handleCheckboxChange() {
+    const checkbox = document.getElementById("myCheckbox") as HTMLInputElement;
+    const statusElement = document.getElementById("status");
+
+    if (checkbox && statusElement) {
+        const status = checkbox.checked ? "Seleccionado" : "No seleccionado";
+        statusElement.textContent = "Estado: " + status;
+    }
+}
+*/ 
